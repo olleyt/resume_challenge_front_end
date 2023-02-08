8a56c67c1f2a341bd3429421eec98f3106170581
Olley <goodnewsolley@gmail.com>
2023-02-08 14:45:24 +1100

    replaces button to label as a container for visitor counter in the Cypress test my-site-e2e.cy.ts

--- cypress/e2e/my-site-e2e.cy.ts
+++ cypress/e2e/my-site-e2e.cy.ts
@@ -3,22 +3,22 @@ describe('Visitor Counter Test', () => {
     beforeEach(() => {
       cy.visit('https://test.coolarchitect.link/')
       cy.wait(3000) // wait for 3 seconds
-      cy.get('.btn-info').as('counterContainer')
-      cy.get('@counterContainer').then(($btn) => {
-        startingValue = parseInt($btn.text())
+      cy.get('.badge.badge-info').as('counterContainer')
+      cy.get('@counterContainer').then(($label) => {
+        startingValue = parseInt($label.text())
       })
     })
     it('checks the value of the button info element', () => {
       cy.visit('https://test.coolarchitect.link/')
       cy.wait(3000) // wait for 3 seconds
-      cy.get('@counterContainer').then(($btn) => {
-        const value = parseInt($btn.text())
+      cy.get('@counterContainer').then(($label) => {
+        const value = parseInt($label.text())
         expect(value).to.equal(startingValue + 1)
       })
       cy.visit('https://test.coolarchitect.link/')
       cy.wait(3000) // wait for 3 seconds
-      cy.get('@counterContainer').then(($btn) => {
-        const value = parseInt($btn.text())
+      cy.get('@counterContainer').then(($label) => {
+        const value = parseInt($label.text())
         expect(value).to.equal(startingValue + 2)
       })
     })
